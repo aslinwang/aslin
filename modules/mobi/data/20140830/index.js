3b26b80e37a614d7eca57f2312e8c5f4205cdcc6
aslinwang <dreamjet.wang@gmail.com>
2014-08-30 17:12:13 +0800

    add data 20140830

---
+++ modules/mobi/data/20140830/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  dir : '20140830/',
+  title : 'Mobile Web Refactor',
+  author : 'aslinwang',
+  urls : [
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-preparatory-work.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-create-page.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-mobile-layout.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-reset-style.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-uniform-and-center.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-slider-animation.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-icons.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-scroll.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-scroll-with-swipe.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-sidebar-menu.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-modal.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-popup.html',
+    'http://www.w3cplus.com/mobile/mobile-terminal-refactoring-slider.html'
+  ]
+};
