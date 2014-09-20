07779dbbc9b6b367c636be18410f5c41355b27a3
aslinwang <dreamjet.wang@gmail.com>
2014-09-20 19:06:27 +0800

    add mobi data 20140920

---
+++ modules/mobi/data/20140920/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  dir : '20140920/',
+  title : '奇舞周刊74期',
+  author : 'aslinwang',
+  urls : [
+    'http://www.css88.com/archives/5126',
+    'http://qianduanblog.com/post/js-learning-34-en-decodeuri-en-decodeuricomponent-un-escape-btoa-atob.html',
+    'http://qianduanblog.com/post/js-learning-35-js-jsonp-var-realize-the-front-cross-domain.html',
+    'http://www.phase2technology.com/blog/used-and-abused-css-inheritance-and-our-misuse-of-the-cascade/',
+    'http://www.w3cplus.com/css/introducing-am-css.html',
+    'http://diao.it/blog/2013/06/06/the-code-side-of-color/',
+    'https://hacks.mozilla.org/2014/09/single-div-drawings-with-css/?utm_source=CSS-Weekly&utm_campaign=Issue-128&utm_medium=email',
+    'http://wileam.com/iphone-6-screen-cn/',
+    'https://github.com/iischajn/iischajn.github.com/issues/1',
+    'http://www.sencha.com/blog/apple-shows-love-for-html5-with-ios-8',
+    'https://github.com/fex-team/http2-spec/blob/master/HTTP2%E4%B8%AD%E8%8B%B1%E5%AF%B9%E7%85%A7%E7%89%88(06-29).md'
+  ]
+};
